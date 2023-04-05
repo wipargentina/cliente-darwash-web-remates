3f4a06287cb0c6abcf61bfd18a6fdea959b92e69
German Sayago <german.sayago@gmail.com>
2023-04-05 18:40:12 -0300

    add fixes errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,13 @@ module.exports = {
   rules: {
     semi: ['error', 'always'],
     ' prettier/prettier': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
   },
 };
