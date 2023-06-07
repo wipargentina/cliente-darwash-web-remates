f486998c1ff0e3da8c60b63dc4bc69d1bec1aace
German Sayago <german.sayago@gmail.com>
2023-06-07 18:48:41 -0300

    added gtm, pixel fb and data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,10 @@
+import { useEffect } from 'react';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { GTM_ID, pageview } from '../lib/gtm';
+
+import { FB_PIXEL_ID, FbCapiProvider } from '../context/FbCapiContext';
+
 import Layout from 'layout';
 
 import '@styles/app.scss';
@@ -7,10 +14,53 @@ import locale from 'moment/locale/es';
 moment.updateLocale('es', locale);
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on('routeChangeComplete', pageview);
+    return () => {
+      router.events.off('routeChangeComplete', pageview);
+    };
+  }, [router.events]);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
+      {/* Global Site Code Pixel - Facebook Pixel */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${FB_PIXEL_ID});
+          `,
+        }}
+      />
+      <FbCapiProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </FbCapiProvider>
+    </>
   );
 }
 
